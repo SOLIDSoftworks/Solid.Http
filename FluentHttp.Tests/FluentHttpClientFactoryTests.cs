be295a3f32607b1fec134464ede89df45bc9e781
Gísli Konráð Björnsson <gisli.konrad@gmail.com>
2017-06-25 11:16:22 +0000

    changes unit testing framework to xunit

--- FluentHttp.Tests/FluentHttpClientFactoryTests.cs
+++ FluentHttp.Tests/FluentHttpClientFactoryTests.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Extensions.Configuration;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using System;
 using System.Collections.Generic;
@@ -7,11 +6,10 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using FluentAssertions;
+using Xunit;
 
 namespace FluentHttp.Tests
 {
-    [TestClass]
     public class FluentHttpClientFactoryTests
     {
         private Mock<IConfigurationRoot> _configurationMock;
@@ -19,8 +17,7 @@ namespace FluentHttp.Tests
 
         private readonly string _baseUrl = "https://test.uri";
 
-        [TestInitialize]
-        public void Initialize()
+        public FluentHttpClientFactoryTests()
         {
             var sectionMock = new Mock<IConfigurationSection>();
             sectionMock.Setup(s => s["test"]).Returns(_baseUrl);
@@ -29,7 +26,7 @@ namespace FluentHttp.Tests
             _cacheMock = new Mock<IHttpClientCache>();
         }
 
-        [TestMethod]
+        [Fact]
         public void ShouldCreateAbsoluteUrlClient()
         {
             var factory = new FluentHttpClientFactory(_cacheMock.Object, _configurationMock.Object);
@@ -39,10 +36,10 @@ namespace FluentHttp.Tests
             _cacheMock.Verify(c => c.Cache(FluentHttpClientFactory.ABSOLUTE_CLIENT_KEY, It.Is<HttpClient>(http => http.BaseAddress == null)), Times.Once());
             _cacheMock.Verify(c => c.Get(FluentHttpClientFactory.ABSOLUTE_CLIENT_KEY), Times.Once());
 
-            client.Should().NotBeNull();
+            Assert.NotNull(client);
         }
 
-        [TestMethod]
+        [Fact]
         public void ShouldGetCachedAbsoluteUrlClient()
         {
             _cacheMock.Setup(c => c.Exists(FluentHttpClientFactory.ABSOLUTE_CLIENT_KEY)).Returns(true);
@@ -54,32 +51,23 @@ namespace FluentHttp.Tests
             _cacheMock.Verify(c => c.Cache(FluentHttpClientFactory.ABSOLUTE_CLIENT_KEY, It.IsAny<HttpClient>()), Times.Never());
             _cacheMock.Verify(c => c.Get(FluentHttpClientFactory.ABSOLUTE_CLIENT_KEY), Times.Once());
 
-            client.Should().NotBeNull();
+			Assert.NotNull(client);
         }
 
-        [TestMethod]
+        [Fact]
         public void ShouldThrowExceptionIfNoConfiguration()
         {
             var key = "test";
             var factory = new FluentHttpClientFactory(_cacheMock.Object);
-            try
-            {
-                factory.Create(key);
-            }
-            catch(Exception ex)
-            {
-                ex.Should().BeAssignableTo<InvalidOperationException>();
-                var message =
-                    "FluentHttpClientFactory was created with a null configuration." + Environment.NewLine +
-                    "If you are creating FluentHttpClientFactory manually, please provide an IConfigurationRoot in the constructor of FluentHttpClientFactory." + Environment.NewLine +
-                    "If you are calling services.UseFluentHttp(), then change that call to services.UseFluentHttp(configuration) where configuration is the application configuration.";
-                ex.Message.Should().Be(message);
-                return;
-            }
-            Assert.Fail("No exception thrown");
+
+			//var message =
+				//"FluentHttpClientFactory was created with a null configuration." + Environment.NewLine +
+				//"If you are creating FluentHttpClientFactory manually, please provide an IConfigurationRoot in the constructor of FluentHttpClientFactory." + Environment.NewLine +
+				//"If you are calling services.UseFluentHttp(), then change that call to services.UseFluentHttp(configuration) where configuration is the application configuration.";
+            Assert.Throws<InvalidOperationException>(() => factory.Create(key));
         }
 
-        [TestMethod]
+        [Fact]
         public void ShouldCreateConnectionStringClient()
         {
             var key = "test";
@@ -90,10 +78,10 @@ namespace FluentHttp.Tests
             _cacheMock.Verify(c => c.Cache(key, It.Is<HttpClient>(http => http.BaseAddress == new Uri(_baseUrl))), Times.Once());
             _cacheMock.Verify(c => c.Get(key), Times.Once());
 
-            client.Should().NotBeNull();
+			Assert.NotNull(client);
         }
 
-        [TestMethod]
+        [Fact]
         public void ShouldGetCachedConnectionStringClient()
         {
             var key = "test";
@@ -106,7 +94,7 @@ namespace FluentHttp.Tests
             _cacheMock.Verify(c => c.Cache(key, It.IsAny<HttpClient>()), Times.Never());
             _cacheMock.Verify(c => c.Get(key), Times.Once());
 
-            client.Should().NotBeNull();
+			Assert.NotNull(client);
         }
     }
 }
