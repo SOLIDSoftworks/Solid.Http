3476361069beb9f940c17831bcece8f25391e9a5
Gísli Konráð Björnsson <gisli.konrad@gmail.com>
2017-10-13 17:15:09 +0000

    adds FluentHttpClientFactoryBuilder

---
+++ FluentHttp.Core/FluentHttpClientFactoryBuilder.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FluentHttp
+{
+    public class FluentHttpClientFactoryBuilder
+    {
+        private ServiceCollection _collection;
+        private IFluentHttpSetup _setup;
+        
+        public FluentHttpClientFactoryBuilder()
+        {
+            _collection = new ServiceCollection();
+            _setup = _collection.AddFluentHttp();
+        }
+
+        public FluentHttpClientFactoryBuilder AddConfiguration(IConfiguration configuration)
+        {
+            _collection.AddSingleton<IConfiguration>(configuration);
+            return this;
+        }
+
+        public FluentHttpClientFactoryBuilder Setup(Action<IFluentHttpSetup> add)
+        {
+            add(_setup);
+            return this;
+        }
+
+        //public FluentHttpClientFactoryBuilder Configure(Action<IFluentHttpOptions> configure)
+        //{
+        //    return AddSetup(s => s.Configure(configure));
+        //}
+
+        public IFluentHttpClientFactory Build()
+        {
+            var provider = _collection.BuildServiceProvider();
+            return provider.GetService<IFluentHttpClientFactory>();
+        }
+    }
+}
