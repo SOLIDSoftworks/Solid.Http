b2c9d49efceabdf0a140998208eb400c575d80de
Gísli Konráð Björnsson <gisli.konrad@gmail.com>
2017-10-13 12:30:34 +0000

    adds more xml comments

--- FluentHttp.Core/FactoryExtensions/ConnectionStringExtensions.cs
+++ FluentHttp.Core/FactoryExtensions/ConnectionStringExtensions.cs
@@ -5,8 +5,17 @@ using System.Text;
 
 namespace FluentHttp
 {
+    /// <summary>
+    /// Extensions to create a FluentHttpClient using a connection string
+    /// </summary>
     public static class ConnectionStringExtensions
     {
+        /// <summary>
+        /// Creates a FluentHttpClient using a connection string
+        /// </summary>
+        /// <param name="factory">The IFluentHttpClientFactory</param>
+        /// <param name="connectionStringName">The name of the connection string in the configuration file</param>
+        /// <returns>FluentHttpClient</returns>
         public static FluentHttpClient CreateUsingConnectionString(this IFluentHttpClientFactory factory, string connectionStringName)
         {
             var baseAddress = factory.Configuration.GetConnectionString(connectionStringName);
