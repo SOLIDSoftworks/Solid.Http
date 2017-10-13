f6dfa5a3f6ccbc9bcfbe02b724da5083e2c14456
Gísli Konráð Björnsson <gisli.konrad@gmail.com>
2017-10-13 13:19:55 +0000

    adds more xml comments

--- FluentHttp.Core/RequestExtensions/UrlExtensions.cs
+++ FluentHttp.Core/RequestExtensions/UrlExtensions.cs
@@ -5,8 +5,18 @@ using System.Text.RegularExpressions;
 
 namespace FluentHttp
 {
+    /// <summary>
+    /// UrlExtensions
+    /// </summary>
     public static class UrlExtensions
     {
+        /// <summary>
+        /// Replaces a templated parameter in the url
+        /// </summary>
+        /// <param name="request">The FluentHttpRequest</param>
+        /// <param name="name">The name of the templated parameter</param>
+        /// <param name="value">The value to inject</param>
+        /// <returns>FluentHttpRequest</returns>
         public static FluentHttpRequest WithNamedParameter(this FluentHttpRequest request, string name, string value)
         {
             var url = request.BaseRequest.RequestUri.OriginalString;
@@ -16,6 +26,13 @@ namespace FluentHttp
             return request;
         }
 
+        /// <summary>
+        /// Adds a query parameter to the url
+        /// </summary>
+        /// <param name="request">The FluentHttpRequest</param>
+        /// <param name="name">The name of the query parameter</param>
+        /// <param name="value">The value of the query parameter</param>
+        /// <returns></returns>
         public static FluentHttpRequest WithQueryParameter(this FluentHttpRequest request, string name, string value)
         {
             var url = request.BaseRequest.RequestUri.OriginalString;
