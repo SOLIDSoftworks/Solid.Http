49796a7171175990ef9c27732ef31e1c1dcde37e
Gísli Konráð Björnsson <gisli.konrad@gmail.com>
2017-10-13 12:01:25 +0000

    adds more xml comments

--- FluentHttp.Core/FactoryExtensions/BaseAddressExtensions.cs
+++ FluentHttp.Core/FactoryExtensions/BaseAddressExtensions.cs
@@ -4,14 +4,28 @@ using System.Text;
 
 namespace FluentHttp
 {
+    /// <summary>
+    /// Extensions to create a FluentHttpClient with a base address
+    /// </summary>
     public static class BaseAddressExtensions
     {
-
+        /// <summary>
+        /// Creates a FluentHttpClient with a base address
+        /// </summary>
+        /// <param name="factory">The IFluentHttpClientFactory</param>
+        /// <param name="baseAddress">The base address to use</param>
+        /// <returns>FluentHttpClient</returns>
         public static FluentHttpClient CreateWithBaseAddress(this IFluentHttpClientFactory factory, string baseAddress)
         {
             return factory.CreateWithBaseAddress(new Uri(baseAddress));
         }
 
+        /// <summary>
+        /// Creates a FluentHttpClient with a base address
+        /// </summary>
+        /// <param name="factory">The IFluentHttpClientFactory</param>
+        /// <param name="baseAddress">The base address to use</param>
+        /// <returns>FluentHttpClient</returns>
         public static FluentHttpClient CreateWithBaseAddress(this IFluentHttpClientFactory factory, Uri baseAddress)
         {
             var client = factory.Create();
