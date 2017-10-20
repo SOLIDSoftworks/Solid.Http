74939e5062dbbf6238a334f46fcfecdb229111fe
Gísli Konráð Björnsson <gisli.konrad@gmail.com>
2017-10-20 13:40:57 +0000

    removes commented out code

--- SolidHttp.Core/Implementations/SolidHttpEvents.cs
+++ SolidHttp.Core/Implementations/SolidHttpEvents.cs
@@ -7,16 +7,7 @@ namespace SolidHttp
 {
     internal class SolidHttpEvents : ISolidHttpEventInvoker, IDisposable
     {
-        //private List<EventHandler<SolidHttpClientCreatedEventArgs>> _onClientCreated = new List<EventHandler<SolidHttpClientCreatedEventArgs>>();
-
         public event EventHandler<SolidHttpClientCreatedEventArgs> OnClientCreated;
-        //{
-        //    add
-        //    {
-        //        _onClientCreated.Add(value);
-        //    }
-        //    remove { }
-        //}
         public event EventHandler<SolidHttpRequestCreatedEventArgs> OnRequestCreated;
         public event EventHandler<RequestEventArgs> OnRequest;
         public event EventHandler<ResponseEventArgs> OnResponse;
@@ -29,10 +20,6 @@ namespace SolidHttp
         {
             if (OnClientCreated != null)
                 OnClientCreated(invoker, new SolidHttpClientCreatedEventArgs { Client = client });
-            //_onClientCreated.ForEach(handler =>
-            //{
-            //    handler(invoker, new SolidHttpClientCreatedEventArgs { Client = client });
-            //});
         }
 
         public void InvokeOnRequest(object invoker, HttpRequestMessage request)
