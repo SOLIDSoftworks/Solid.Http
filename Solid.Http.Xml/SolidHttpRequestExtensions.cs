1fe06727f2800e9fb4274cfc508c6fc0bda355cb
HX-Rd <huxleygit@gmail.com>
2018-03-12 17:59:05 +0100

    Added support for DataContractSerializer xml serialization

---
+++ Solid.Http.Xml/SolidHttpRequestExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Solid.Http.Xml
+{
+    internal static class SolidHttpRequestExtensions
+    {
+        public static DataContractSerializerSettings GetXmlSerializerSettings(this SolidHttpRequest request)
+        {
+            return request.BaseRequest.Properties["XmlSerializerSettings"] as DataContractSerializerSettings;
+        }
+    }
+}
