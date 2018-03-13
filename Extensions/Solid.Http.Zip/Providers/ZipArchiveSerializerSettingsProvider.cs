10b78118d96df8b73aaed753d3f1468458fda32f
HX-Rd <huxleygit@gmail.com>
2018-03-13 17:33:47 +0000

    Added support for ZipArchive

---
+++ Extensions/Solid.Http.Zip/Providers/ZipArchiveSerializerSettingsProvider.cs
@@ -0,0 +1,22 @@
+﻿using Solid.Http.Zip.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Text;
+
+namespace Solid.Http.Zip.Providers
+{
+    internal class ZipArchiveSerializerSettingsProvider : IZipSerializerSettingsProvider
+    {
+        private static ZipArchiveMode _mode;
+
+        public ZipArchiveSerializerSettingsProvider(ZipArchiveMode mode)
+        {
+            _mode = mode;
+        }
+        public ZipArchiveMode GetZipSerializerSettings()
+        {
+            return _mode;
+        }
+    }
+}
---
+++ Extensions/Solid.Http.Zip/ZipArchiveResponseDeserializerFactory.cs
@@ -0,0 +1,37 @@
+﻿using Solid.Http.Abstractions;
+using Solid.Http.Zip.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid.Http.Zip
+{
+    internal class ZipArchiveResponseDeserializerFactory : IResponseDeserializerFactory
+    {
+        public ZipArchiveResponseDeserializerFactory(IZipSerializerSettingsProvider provider)
+        {
+            GetSettings = () => provider.GetZipSerializerSettings();
+        }
+
+        internal ZipArchiveResponseDeserializerFactory(ZipArchiveMode settings)
+        {
+            GetSettings = () => settings;
+        }
+
+        private Func<ZipArchiveMode> GetSettings { get; }
+        public Func<HttpContent, Task<T>> CreateDeserializer<T>()
+        {
+            return async (content) =>
+            {
+                var ms = new MemoryStream();
+                await content.CopyToAsync(ms);
+                var arch = new ZipArchive(ms, GetSettings());
+                return (T)((object)arch);
+            };
+        }
+    }
+}
