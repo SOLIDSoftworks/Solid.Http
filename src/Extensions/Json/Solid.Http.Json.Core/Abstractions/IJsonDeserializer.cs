f207b636346601f67fc7d855411adf98cc15cd78
Gísli Konráð Björnsson <gisli.konrad@gmail.com>
2020-05-11 22:18:24 +0000

    Adds XML documentation to Json.Core

--- src/Extensions/Json/Solid.Http.Json.Core/Abstractions/IJsonDeserializer.cs
+++ src/Extensions/Json/Solid.Http.Json.Core/Abstractions/IJsonDeserializer.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Net.Http;
 
 namespace Solid.Http.Json.Core.Abstractions
 {
+    /// <summary>
+    /// An interface that represents a deserializer for JSON <see cref="HttpContent" />.
+    /// </summary>
     public interface IJsonDeserializer : IDeserializer
     {
     }
