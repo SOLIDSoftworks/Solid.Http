3cdace4358e9921e1ffce40d1febcd7da9178fe1
Gísli Konráð Björnsson <gisli.konrad@gmail.com>
2020-05-12 00:48:09 +0000

    Adds xml comments to Solid.Http

--- src/Solid.Http/ServiceCollectionExtensions.cs
+++ src/Solid.Http/ServiceCollectionExtensions.cs
@@ -4,11 +4,27 @@ using System;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
+    /// <summary>
+    /// Extension methods for adding Solid.Http.Core with Solid.Http.Json to an <see cref="IServiceCollection" />.
+    /// </summary>
     public static class Solid_Http_Core_ServiceCollectionExtensions
     {
+        /// <summary>
+        /// Adds Solid.Http to an <see cref="IServiceCollection" />.
+        /// <para>Solid.Http includes Solid.Http.Core and Solid.Http.Json.</para>
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection" /> to add Solid.Http to.</param>
+        /// <returns>The <see cref="IServiceCollection" /> so that additional calls can be chained.</returns>
         public static IServiceCollection AddSolidHttp(this IServiceCollection services) =>
             services.AddSolidHttpCore(builder => builder.AddJson());
 
+        /// <summary>
+        /// Adds Solid.Http to an <see cref="IServiceCollection" />.
+        /// <para>Solid.Http includes Solid.Http.Core and Solid.Http.Json.</para>
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection" /> to add Solid.Http to.</param>
+        /// <param name="buildAction">An action to run on <see cref="SolidHttpBuilder" />.</param>
+        /// <returns>The <see cref="IServiceCollection" /> so that additional calls can be chained.</returns>
         public static IServiceCollection AddSolidHttp(this IServiceCollection services, Action<SolidHttpBuilder> buildAction)
             => services.AddSolidHttp().ConfigureSolidHttp(buildAction);
     }
